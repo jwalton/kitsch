37b88f3181b1dc221f422cdc5f5a3cc9f1e44194
Jason Walton <dev@lucid.thedreaming.org>
2021-11-05 22:03:51 -0400

    feat: Add configdir command.

---
+++ cmd/configdir.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var configDirCmd = &cobra.Command{
+	Use:   "configdir",
+	Short: "Print the location of the configuration directory",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(cfgFolder)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configDirCmd)
+}
