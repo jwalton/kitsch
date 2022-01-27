9a594c53de3311d92437d3dd56e164ab788aa4fc
Jason Walton <dev@lucid.thedreaming.org>
2022-01-27 00:04:13 -0500

    fix: Update generated kubernetes_schema.go file.

--- internal/kitsch/modules/schemas/kubernetes_schema.go
+++ internal/kitsch/modules/schemas/kubernetes_schema.go
@@ -8,6 +8,7 @@ var KubernetesModuleJSONSchema = `{
   "properties": {
     "style": {"type": "string", "description": ""},
     "template": {"type": "string", "description": ""},
+    "type": {"type": "string", "description": "Type is the type of this module.", "enum": ["kubernetes"]},
     "symbol": {"type": "string", "description": "Symbol is a symbol to show if a Kubernetes context is detected.  Defaults to \"☸ \""},
     "contextAliases": {"type": "object", "description": "ContextAliases is a map where keys are context names and values are the value we want to show.  If the value is an empty string, we will not show anything.", "additionalProperties": {"type": "string", "description": ""}},
     "configFile": {"type": "string", "description": "ConfigFile is the path to the kubectl config file.  Defaults to \"~/.kube/config\"."}
